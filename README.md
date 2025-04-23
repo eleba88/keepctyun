3e8414231600f6bf70fb1434c2cdfdeb097b2995
alexander <alexander@518100.xyz>
2025-04-23 21:17:06 +0800

    update

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
-# 天冀云电脑Docke保活
-## 🚀 如何使用？
+# 🚀 天冀云电脑Docke保活
 ### 1️⃣ 推荐Docker部署
 docker-compose.yml
 ```
 services:
   ctyun:
-    image: ghcr.nju.edu.cn/eleba88/keepctyun:latest
+    image: keepctyun:latest
     container_name: ctyun
     restart: always
+    volumes:
+      - './ctyun:/usr/src/server/data'
 ```
 ### 2️⃣ 首次需要扫码
 ```
+创建一个目录放浏览器浏览器数据 puppeteer这个镜像的用户及组是10042:999
+$ mkdir ctyun && sudo chown -R 10042:999 ctyun
 $ docker-compose up -d
 $ docker logs -f ctyun
 ```
-扫码登陆即可,后续会直接登陆
+第一次需要扫码登陆即可,后续会直接登陆
--- server/ctyun.js
+++ server/ctyun.js
@@ -1,12 +1,47 @@
 const Browser = require('./Browser');
+const path = require('path');
 const QRCode = require('qrcode');
+const dataPath = path.join(__dirname, 'data');
+
+function getTimestamp() {
+  const now = new Date();
+  return now.toLocaleString('zh-CN', { hour12: false });
+}
+
+function logger(...args) {
+  console.log(`[${getTimestamp()}]`, ...args);
+}
 
 async function loginSubmit(page) {
-  console.log('尝试检查是否进入主界面');
+  logger('尝试检查是否进入主界面');
   await page.waitForSelector('div.desktop-main-entry', { visible: true });
-  console.log('厉害了铁子, 进入了主界面');
+  logger('厉害了铁子, 进入了主界面');
   await page.click('div.desktop-main-entry');
-  console.log('点击了 "进入" 按钮');
+  logger('点击了 "进入" 按钮');
+}
+
+async function reloadQrCode(page) {
+  const buttons = await page.$$('button.el-button.el-button--primary.el-button--small');
+  for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    const isParentVisible = await page.evaluate(button => {
+      const parent = button.closest('div');
+      const style = window.getComputedStyle(parent);
+      return style.display !== 'none' && style.visibility !== 'hidden' && style.opacity > 0;
+    }, button);
+    if (isParentVisible) {
+      const isButtonVisible = await page.evaluate(button => {
+        const style = window.getComputedStyle(button);
+        return style.display !== 'none' && style.visibility !== 'hidden' && style.opacity > 0;
+      }, button);
+
+      if (isButtonVisible) {
+        await button.click();
+        console.log('按钮已点击');
+        break;
+      }
+    }
+  }
 }
 
 async function handleApiResponse(page, response) { 
@@ -15,20 +50,26 @@ async function handleApiResponse(page, response) {
     const method = response.request().method();
     if (!['GET', 'POST'].includes(method)) return;
     if (url === 'https://desk.ctyun.cn:8810/api/auth/client/qrCode/genData') {
-      console.log(`发现需要扫码登陆`);
+      logger(`发现需要扫码登陆`);
       const resp = await response.json()
       let qrCodeId = resp?.data?.qrCodeId;
       let qrUrl = `https://desk.ctyun.cn:443/selforder/#/login-confirm?qrCodeId=${qrCodeId}&loginMode=1`
-      let code = await QRCode.toString(qrUrl, { type: 'terminal',  errorCorrectionLevel: 'L'});
+      let code = await QRCode.toString(qrUrl, { type: 'terminal', errorCorrectionLevel: 'L' });
       console.log(code);
-      console.log(`若二维码显示异常请打开以下地址进行扫码:\n https://www.olzz.com/qr/?text=${qrUrl}`);
+      logger(`或打开以下链接:\n https://www.olzz.com/qr/?text=${encodeURIComponent(qrUrl)}`);
     } else if (url === 'https://desk.ctyun.cn:8810/api/desktop/client/list') {
       await loginSubmit(page);
     } else if (url === 'https://desk.ctyun.cn:8810/api/desktop/client/connect') {
       const resp = await response.json();
-      console.log(`设备登陆状态: ${resp?.data?.desktopInfo?.status}`);
+      logger(`设备登陆状态: ${resp?.data?.desktopInfo?.status}`);
     } else if (url === 'https://desk.ctyun.cn:8810/api/auth/client/logout') {
-      console.log(`当前设备被挤下线了`);
+      logger(`当前设备被挤下线了`);
+    } else if (url.includes('https://desk.ctyun.cn:8810/api/auth/client/qrCode/getStatus')) { 
+      const resp = await response.json();
+      if ('expire' === resp.data.codeStatus) {
+        logger(`二维码已超时，准备刷新`);
+        await reloadQrCode(page);
+      }
     }
   } catch (error) {
     console.log('onResponse error', error);
@@ -38,6 +79,7 @@ async function handleApiResponse(page, response) {
 async function main() {
   const browser = new Browser({
     userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+    dataPath,
     headless: true
   });
   await browser.launch();
